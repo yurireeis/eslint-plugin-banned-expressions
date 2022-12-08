5e637ff810a57709886fa6794c7171b8c3e2295a
Yuri Reis <yuri.reis@msn.com>
2022-12-08 01:15:35 -0300

    feat: project kickstart

---
+++ tests/lib/rules/banned-expressions.js
@@ -0,0 +1,64 @@
+const { RuleTester } = require("eslint");
+const rule = require("../../../lib/rules/banned-expressions.js");
+
+const settings = {
+  bannedExpressions: [
+    {
+      value: ".forEach",
+      exp: ".forEach",
+      suggestion: "use map instead of forEach",
+    },
+    {
+      value: "Date",
+      exp: "new Date((.{0,}))",
+      suggestion: "use map instead of forEach",
+    },
+  ],
+};
+
+const parserOptions = {
+  ecmaVersion: 2015
+};
+
+const ruleTester = new RuleTester({ settings, parserOptions });
+// const ruleTesterNoBannedExpression = new RuleTester();
+
+ruleTester.run("no-banned-expressions-check", rule, {
+  valid: [
+    {
+      code: "// for each of us",
+    },
+    {
+      code: "// today is a new date",
+    },
+
+    {
+      code: "// avoid using .forEach",
+    },
+    {
+      code: "// avoid using new Date()",
+    },
+  ],
+  invalid: [
+    {
+      code: "new Date()",
+      errors: [{ messageId: "banned-exp" }],
+    },
+    {
+      code: "array.forEach",
+      errors: [{ messageId: "banned-exp" }],
+    },
+  ],
+});
+
+// ruleTesterNoBannedExpression.run("banned-expressions-check", rule, {
+//   valid: [
+//     {
+//       code: "try { foo() } catch (e) { bar() }",
+//     },
+//     {
+//       code: "try { foo() } catch (e) {}",
+//     },
+//   ],
+//   invalid: [],
+// });
